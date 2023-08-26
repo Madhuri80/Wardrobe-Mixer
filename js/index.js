116d24278d556ab7287a479d474ccbd26fd7cd84
Madhuri80 <127690787+Madhuri80@users.noreply.github.com>
2023-08-26 12:33:44 -0500

    edited

--- js/index.js
+++ js/index.js
@@ -1,12 +1,7 @@
 document.getElementById('startButton').addEventListener('click', function() {
     window.location.href = 'second-screen.html';
 });
-$(document).ready(function() {
-    var currentScreen = 2; // Starting on the second screen
 
-    $('.container').on('swipeleft', function() {
-        if (currentScreen === 2) {
-            window.location.href = 'third-screen.html'; // Navigate to third screen
-        }
-    });
+document.getElementById('skip').addEventListener('click', function() {
+    window.location.href = 'signup.html';
 });
