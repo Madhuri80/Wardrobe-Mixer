8bd33d08ee519d92a569b663c2c2d22298b1f3bc
Madhuri80 <127690787+Madhuri80@users.noreply.github.com>
2023-08-26 17:17:26 -0500

    js for rise up your style

---
+++ js/7riseup.js
@@ -0,0 +1,6 @@
+document.getElementById('back').addEventListener('click', function() {
+    window.location.href = '6letsgetstarted.html';
+});
+document.getElementById('skip').addEventListener('click', function() {
+    window.location.href = '8yourloc.html';
+});
