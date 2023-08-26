e301ad4dbc4a36125eb30055e631b83d8449a048
Madhuri80 <127690787+Madhuri80@users.noreply.github.com>
2023-08-26 18:17:19 -0500

    back and next updated

--- js/2onboarding.js
+++ js/2onboarding.js
@@ -1,6 +1,9 @@
 console.log("Index filed #2 loaded successfully")
 
 
-document.getElementById('skip').addEventListener('click', function() {
+document.getElementById('onboardskip').addEventListener('click', function() {
     window.location.href = '5signup.html';
+});
+document.getElementById('onboardnext').addEventListener('click', function() {
+    window.location.href = '3onboarding.html';
 });
--- js/4onboarding.js
+++ js/4onboarding.js
@@ -1,8 +1,8 @@
 console.log("Index filed #3 loaded successfully")
 
 document.getElementById('onboardback').addEventListener('click', function() {
-    window.location.href = '2onboarding.html';
+    window.location.href = '3onboarding.html';
 });
 document.getElementById('onboardnext').addEventListener('click', function() {
-    window.location.href = '4onboarding.html';
+    window.location.href = '5signup.html';
 });
