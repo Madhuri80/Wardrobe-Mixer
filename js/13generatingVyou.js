5af8fc0bde0fd6a26e2f09f33a2355b3f6c097e0
Madhuri80 <127690787+Madhuri80@users.noreply.github.com>
2023-08-27 16:39:59 -0500

    loader virtual you

--- js/13generatingVyou.js
+++ js/13generatingVyou.js
@@ -1,16 +1,5 @@
 const loadingContainer = document.getElementById('loading-container');
 
-function createLoadingAnimation(dotsCount) {
-    for (let i = 0; i < dotsCount; i++) {
-        const dot = document.createElement('div');
-        dot.className = 'loading-dot';
-        dot.classList.add(i === 0 ? 'black' : 'grey');
-        loadingContainer.appendChild(dot);
-    }
-}
-
-createLoadingAnimation(20); // Adjust the number of dots as needed
-
 
 
 
