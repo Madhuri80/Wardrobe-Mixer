a625cfb4e8869b444b67173f467d47bd95165b8c
Madhuri80 <127690787+Madhuri80@users.noreply.github.com>
2023-08-27 00:17:46 -0500

    signup with js

---
+++ js/5signup.js
@@ -0,0 +1,10 @@
+console.log("Index filed loaded successfully")
+
+
+document.getElementById('social-button1').addEventListener('click', function() {
+    window.location.href = '6letsgetstarted.html';
+});
+document.getElementById('social-button2').addEventListener('click', function() {
+    window.location.href = '6letsgetstarted.html';
+});
+
