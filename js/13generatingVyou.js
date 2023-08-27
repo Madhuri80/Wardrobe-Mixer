1394985e8fb3c818e2d8147baacbc240e8e74b65
Madhuri80 <127690787+Madhuri80@users.noreply.github.com>
2023-08-27 16:48:29 -0500

    Java script for Avatar

--- js/13generatingVyou.js
+++ js/13generatingVyou.js
@@ -1,5 +1,11 @@
 const loadingContainer = document.getElementById('loading-container');
+const loader = document.querySelector('.loading-container');
 
+// Simulate the spinning loader animation
+setTimeout(() => {
+    // Redirect to the new page after 5 seconds
+    window.location.href = '14youravatar.html'; // Replace 'avatar.html' with your desired page URL
+}, 5000); // 5 seconds delay
 
 
 
